e97e52f2fe2448a89c097c3193f2ffb893ea2218
Francisco Andrade <fdfandrade@gmail.com>
2017-01-18 23:55:38 +0000

    Adding support to sendgrid

--- template.rb
+++ template.rb
@@ -1,48 +1,75 @@
-RAILS_REQUIREMENT = "~> 5.0.1"
+RAILS_REQUIREMENT = '~> 5.0.1'
 
 def apply_template!
   start_message
   assert_minimum_rails_version
+  #assert_git
+  #assert_heroku
   #assert_valid_options
   #assert_postgresql
   add_template_repository_to_source_path
 
-  template "Gemfile.tt",   :force => true
-  template "README.md.tt", :force => true
+  template 'Gemfile.tt',   :force => true
+  template 'README.md.tt', :force => true
 
   rails_command 'db:setup'
 
-  rails_command "g devise:install"
-  rails_command "g devise user -e"
+  rails_command 'g devise:install'
+  rails_command 'g devise user -e'
 
-  rails_command "db:migrate"
+  rails_command 'db:migrate'
 
-  rails_command "g devise:views user -e"
-  rails_command "g controller home index -e"
+  rails_command 'g devise:views user -e'
+  rails_command 'g controller home index -e'
 
   # add first language, remember to copy helper and application controller
-  rails_command "gettext:add_language LANGUAGE=en"
+  rails_command 'gettext:add_language LANGUAGE=en'
 
-  directory "config", "config", :force => true
+  # configure puma
+  copy_file 'Procfile', 'Procfile', :force => true
 
-  directory "app/assets/javascripts", "app/assets/javascripts", :force => true
-  directory "app/assets/stylesheets", "app/assets/stylesheets", :force => true
-  
-  directory "app/controllers", "app/controllers", :force => true
-  
-  directory "app/helpers", "app/helpers", :force => true
-  
-  directory "app/views", "app/views", :force => true
+  directory 'config', 'config', :force => true
 
+  directory 'app/assets/javascripts', 'app/assets/javascripts', :force => true
+  directory 'app/assets/stylesheets', 'app/assets/stylesheets', :force => true
+  
+  directory 'app/controllers', 'app/controllers', :force => true
+  
+  directory 'app/helpers', 'app/helpers', :force => true
+  
+  directory 'app/views', 'app/views', :force => true
+
+  inject_into_file 'config/environments/development.rb', after: "config.action_mailer.perform_caching = false\n" do <<-'RUBY'
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+    RUBY
+  end
+
+  inject_into_file 'config/environments/production.rb', after: "config.action_mailer.perform_caching = false\n" do <<-'RUBY'
+  config.action_mailer.default_url_options = { host: 'https://your-app-name.herokuapp.com/' }
+    RUBY
+  end
+  
   # create the git repo and the first commit
+  git :init
 
   # create heroku app
+  run 'heroku create'
+
+  git add: '.'
+  run 'git commit -m "Initialize repository!"'
+
+  # we need to do a first successfull installation
+  run 'git push heroku master'
 
   # run db:setup
+  run 'heroku run rails db:migrate'
 
   # run db:migrate
+  run 'heroku ps:scale web=1'
 
-  # ...
+  # configure sendgrid
+  run 'heroku addons:create sendgrid:starter'
+
 end
 
 def start_message
@@ -59,8 +86,8 @@ def assert_minimum_rails_version
   rails_version = Gem::Version.new(Rails::VERSION::STRING)
   return if requirement.satisfied_by?(rails_version)
 
-  prompt = "This template requires Rails #{RAILS_REQUIREMENT}. "\
-           "You are using #{rails_version}. Continue anyway?"
+  prompt = 'This template requires Rails #{RAILS_REQUIREMENT}. '\
+           'You are using #{rails_version}. Continue anyway?'
   exit 1 if no?(prompt)
 end
 
@@ -73,13 +100,13 @@ end
 # In that case, use `git clone` to download them to a local temporary dir.
 def add_template_repository_to_source_path
   if __FILE__ =~ %r{\Ahttps?://}
-    source_paths.unshift(tempdir = Dir.mktmpdir("rails-template-"))
+    source_paths.unshift(tempdir = Dir.mktmpdir('rails-template-'))
     at_exit { FileUtils.remove_entry(tempdir) }
     git :clone => [
-      "--quiet",
-      "https://github.com/mattbrictson/rails-template.git",
+      '--quiet',
+      'https://github.com/mattbrictson/rails-template.git',
       tempdir
-    ].map(&:shellescape).join(" ")
+    ].map(&:shellescape).join(' ')
   else
     source_paths.unshift(File.dirname(__FILE__))
   end
